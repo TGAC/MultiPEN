20b5a78fcd69a8c54bc805a2fcb306ef59a90454
PerlaTroncosoRey <perlatroncosorey@gmail.com>
2016-05-16 16:44:40 +0100

    includes MIT_NetworkAnalysis script to generate random graphs

--- scripts/randomGraph.m
+++ scripts/randomGraph.m
@@ -3,13 +3,19 @@
 %       p  probability
 %       d  node degree
 
-%It uses Erdos-Renyi Random Graph
+%Using "Erdos-Renyi Random Graph" resource
 %  http://www.mathworks.com/matlabcentral/fileexchange/4206-erdos-renyi-random-graph
 
 addpath 'ErdosRenyi_RandomGraph/'
-
 
 % example: G = erdosRenyi(40,0.01,4);
 G = erdosRenyi(n,p,d);
 
 plotGraphBasic(G,6,1);
+
+
+%Using "MIT Network Analysis" resource
+%  http://strategic.mit.edu/downloads.php?page=matlab_networks
+
+addpath 'MIT_NetworkAnalysis/'
+A = random_graph(5,0.5,10);  %adjacency matrix A
