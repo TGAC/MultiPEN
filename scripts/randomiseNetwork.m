ae70edb49029630d83b60b5b155736647fceab9f
PerlaTroncosoRey <perlatroncosorey@gmail.com>
2016-05-18 10:50:44 +0100

    added test to randomly swap edges for testing influence of network in gene selection

--- scripts/randomiseNetwork.m
+++ scripts/randomiseNetwork.m
@@ -1,4 +1,4 @@
-function randomNetwork = randomizeNetwork(network, elementToChange, change, percentage)
+function randomNetwork = randomiseNetwork(E, elementToChange, change, percentage)
 %Randomise the network according to the type of change specified
 %   gene2geneNetwork
 
@@ -6,17 +6,17 @@ function randomNetwork = randomizeNetwork(network, elementToChange, change, perc
 %load('/Users/troncosp/Documents/Projects/EscapePilot/inputForGenePEN/gene2geneNetwork.mat')
 
 %get general features
-[numberEdges,~] = size(network);
-numberNodes = numel(unique([network.fromIndx; network.toIndx]));
+[numberEdges,~] = size(E);
+numberNodes = numel(unique([E(:,1); E(:,2)]));
 fprintf('ORIGINAL NETWORK FEATURES \n')
 fprintf('Number of Edges: %i\n', numberEdges)
 fprintf('Number of Nodes: %i\n', numberNodes)
 
 
 switch elementToChange
-    case 'edges'
+    case 'edge'
         totalElements = numberEdges;
-    case 'nodes'
+    case 'node'
         totalElements = numberNodes;
 end
 
@@ -25,19 +25,56 @@ nChanges = ceil(totalElements*percentage);
 switch change
     case 'random'        
         p = randperm(totalElements,nChanges);
-        newToNodes = p(end:-1:1)
-        from = network.from;
-        fromIndex = network.fromIndx;        
-        score = network.score;        
-        to = network.to;
-        to(p) = network.to(newToNodes);
-        toIndex = network.toIndx;
-        toIndex(p) = network.toIndx(newToNodes);
-        randomNetwork = table(from,to, score, fromIndex, toIndex, ...
-            'VariableNames', {'from' 'to' 'score' 'fromIndx' 'toIndx'});
+        newToNodes = p(end:-1:1);
+        from = E(:,1);        
+        score = E(:,3);                        
+        to =  E(:,2);
+        to(p) = E(newToNodes,2);
+        randomNetwork = [from to score];
     case 'leaf'
     case 'hub'
 end
 
 
 
+
+% function randomNetwork = randomizeNetwork(network, elementToChange, change, percentage)
+% %Randomise the network according to the type of change specified
+% %   gene2geneNetwork
+% 
+% %example with network from escape:
+% %load('/Users/troncosp/Documents/Projects/EscapePilot/inputForGenePEN/gene2geneNetwork.mat')
+% 
+% %get general features
+% [numberEdges,~] = size(network);
+% numberNodes = numel(unique([network.fromIndx; network.toIndx]));
+% fprintf('ORIGINAL NETWORK FEATURES \n')
+% fprintf('Number of Edges: %i\n', numberEdges)
+% fprintf('Number of Nodes: %i\n', numberNodes)
+% 
+% 
+% switch elementToChange
+%     case 'edge'
+%         totalElements = numberEdges;
+%     case 'node'
+%         totalElements = numberNodes;
+% end
+% 
+% nChanges = ceil(totalElements*percentage);
+% 
+% switch change
+%     case 'random'        
+%         p = randperm(totalElements,nChanges);
+%         newToNodes = p(end:-1:1)
+%         from = network.from;
+%         fromIndex = network.fromIndx;        
+%         score = network.score;        
+%         to = network.to;
+%         to(p) = network.to(newToNodes);
+%         toIndex = network.toIndx;
+%         toIndex(p) = network.toIndx(newToNodes);
+%         randomNetwork = table(from,to, score, fromIndex, toIndex, ...
+%             'VariableNames', {'from' 'to' 'score' 'fromIndx' 'toIndx'});
+%     case 'leaf'
+%     case 'hub'
+% end
