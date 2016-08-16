05b9779093c10ea7b856853f9848aacef32f912f
PerlaTroncosoRey <perlatroncosorey@gmail.com>
2016-08-16 17:11:49 +0100

    added reads input data (instead of passing file names to cross_validation.m); adds fold change to feature selection, as well as expression data

--- scripts/MultiPEN.m
+++ scripts/MultiPEN.m
@@ -31,7 +31,7 @@ switch analysisType
     case 'cross_validation'
         % cross validation needs parameters: 
         % X, E, Y, lambdas, folds, numIter (optional)
-        if ~((nargin == 5) || (nargin == 6))
+        if ~((length(varargin) == 5) || (length(varargin) == 6))
             error('The number of arguments is incorrect')
         else
             X = varargin{1};
@@ -45,12 +45,101 @@ switch analysisType
                 numIter = varargin{6};
             end
         end
+        
+    case 'featureSelection'
+        % featureSelection needs parameters:
+        %X, E, Y, lambda, features, sampleAnnot, numIter (optional)
+        if ~((length(varargin) == 6) || (length(varargin) == 7))
+            error('The number of arguments is incorrect')
+        else
+            X = varargin{1};
+            E = varargin{2};
+            Y = varargin{3};
+            lambda = varargin{4};
+            features = varargin{5};
+            samples = varargin{6};
+            if length(varargin) == 6
+                numIter = 100;
+            else
+                numIter = varargin{7};
+            end
+        end
     otherwise
-        error('The type of analysis is incorrect')
+        error('Please specify a valid analysis')
 end
     
+%% READ INPUT DATA
+%  Input arguments passed from the system prompt will be received as strings
+%  Thus, converting strings to double if required 
 
-%% Add library's path
+% X - expression data n-by-p
+exist X 'var'
+if ans
+    if ~isa(X,'double')
+        Xfile = X;     
+        X = load(Xfile, '-ascii');
+    end
+end
+ 
+%E - interaction matrix (network edges)
+exist E 'var'
+if ans
+    if ~isa(E,'double')
+        Efile = E;    
+        E = load(Efile, '-ascii');
+    end
+end
+
+
+%class - Y
+exist Y 'var'
+if ans
+    if ~isa(Y,'double')
+        Yfile = Y;
+        Y = load(Yfile, '-ascii');
+    end
+end
+
+
+% %lambdas for cross validation
+% exist lambdas 'var'
+% if ans
+%     if ~isa(lambdas, 'double')
+%         lambdas = str2num(lambdas);   
+%     end
+% end
+
+
+% %folds for cross validation
+% exist folds 'var'
+% if ans
+%     if ~isa(folds, 'double')
+%         folds = str2num(folds);
+%     end
+% end
+
+% Feature annotation
+exist features 'var'
+if ans
+    if ~isa(features,'double')
+        featureAnnotfile = features;           
+        features = table2cell(readtable(featureAnnotfile, 'ReadVariableNames', false));
+    end
+end
+
+% Sample annotation
+exist samples 'var'
+if ans
+    if ~isa(samples,'double')
+        samplesFile = samples;           
+        samples = table2cell(readtable(samplesFile, 'ReadVariableNames', false));
+    end
+end
+
+
+
+
+%% ADD PATH TO LIBRARIES
 addpath('Libraries/')
 addpath('Libraries/fastGapFill/')
 addpath('Libraries/gaimc/')
@@ -67,62 +156,87 @@ if numIter == 0
 end
 
 switch analysisType
-    case 'cross_validation'
-        outputDir = 'output_MultiPEN/cross_validation/';
+    case 'hierarchical_clustering'
         
+    
+    case 'cross_validation'                
         %cross_validation for different lambdas
         fprintf('Performing cross validation... \n')        
         [~, ~, ~, outcome_stats] = cross_validation(X, E, Y, lambdas, folds, numIter);
         stats = table(outcome_stats(:,1), outcome_stats(:,2), outcome_stats(:,3), outcome_stats(:,4), outcome_stats(:,5), outcome_stats(:,6), ...
             'VariableNames', {'lambda' 'LCC' 'std_LCC' 'selected' 'AUC' 'std_AUC'});
         
-        if saveResults            
+        if ~strcmp(saveResults,'false')
+            if strcmp(saveResults, 'true')
+                outputDir = 'output_MultiPEN/cross_validation/';
+            else
+                outputDir = saveResults;
+            end
+            
+            %check if output directory exists
+            if exist(outputDir, 'dir') ~= 7
+                mkdir(outputDir)
+            end
+                        
+            % Statistics for cross validation
+            fileName = [outputDir 'cross-validation_stats.txt'];
+            writetable(stats, fileName, 'delimiter', '\t')
+            fprintf('\tStatistics are saved to file: \n\t%s\n', ...
+                fileName)
+            fprintf('Following are the statistics for cross validation\n')
+            display(stats)
+                        
+        end
+        
+        MP = outcome_stats;
+        
+          
+    case 'featureSelection'
+        %Feature selection for a specific lambda
+        fprintf('Performing feature selection... \n')
+        %FS is a table with columns: [name, weight, ranking]
+        [FS, vts] = featureSelection(X, E, Y, features, lambda, numIter);
+        
+        %compute fold change
+        [FS, higherControl, higherCases] = foldChange(FS, X, Y, samples);
+        
+        % WRITE feature selection (FS table) to file
+        if ~strcmp(saveResults,'false')
+            if strcmp(saveResults, 'true')
+                outputDir = 'output_MultiPEN/feature_selection/';
+            else
+                outputDir = saveResults;
+            end
+            
             %check if output directory exists
             if exist(outputDir, 'dir') ~= 7
                 mkdir(outputDir)
             end
             
+            %feature's ranking
+            %[name weight ranking]
+            fileName = [outputDir 'MultiPEN-Rankings_lambda' num2str(lambda)];
+            fprintf('Writing feature selection to file: \n\t%s\n',fileName)
+            writetable(FS, [fileName '.txt'], 'delimiter', '\t');
+            save([fileName '.mat'], 'FS')
             
-            % Statistics for cross validation
-            writetable(stats, [outputDir 'crossValidationStats.csv'])
-            fprintf('\tStatistics are saved to file: \n\t%s\n', ...
-                [outputDir 'crossValidationStats.csv'])
-            fprintf('Following are the statistics for cross validation\n')
-            display(stats)
+            %intercept learnt from feature selection
+            fileName = [outputDir 'MultiPEN-vts_lambda' num2str(lambda)];            
+            dlmwrite([fileName '.txt'], vts, 'delimiter', '\t');    
+            
+            %% write separate tables for higherControl an higherCases
+            fileName = [outputDir 'MultiPEN-Rankings_lambda' num2str(lambda) '_genes-higher-in-control.txt'];
+            fprintf('Writing feature selection to file: \n\t%s\n',fileName)
+            writetable(higherControl, fileName, 'delimiter', '\t');
+            
+            fileName = [outputDir 'MultiPEN-Rankings_lambda' num2str(lambda) '_genes-higher-in-cases.txt'];
+            fprintf('Writing feature selection to file: \n\t%s\n',fileName)
+            writetable(higherCases, fileName, 'delimiter', '\t');
+            %
         end
-        
-        MP = outcome_stats;
-        
-        %     %% Save feature feature selection results for current lambda
-%     %check if drectory exists
-%     if exist(dirResults, 'dir') ~= 7
-%         mkdir(dirResults)
-%     end    
-%     %save results
-%     fileName = [dirResults 'weights_lambda' num2str(lambda) '.mat'];
-%     fprintf('Saving results in file: \n    %s\n', fileName)    
-%     save(fileName, 'weights')
-%     dlmwrite([dirResults 'weights_lambda' num2str(lambda) '.txt'],weights,'delimiter','\t');
-%     %vts parameter
-%     fileName = [dirResults 'vts_lambda' num2str(lambda) '.mat'];
-%     fprintf('Saving results in file: \n    %s\n', fileName)    
-%     save(fileName, 'vts')
-%     dlmwrite([dirResults 'vts_lambda' num2str(lambda) '.txt'],vts,'delimiter','\t');
-
-    case 'GenePEN'        
-        fprintf('Performing gene selection with GenePEN... \n')        
-        [~, rankedGenes] = geneSelection(X, E, Y, featureNames, lambdas, outputDir, geneIndex, numIter);
-        %show top 10 rankings        
-        top10 = rankedGenes(rankedGenes.ranking < 11,:);
-        %sort genes by weight then by name
-        sortedTop10 = sortrows(top10, {'ranking','shortName'});        
-        fprintf('The top ranked genes are:\n')        
-        display(sortedTop10(:,[4 2 3 1 5]))
-        
-    case 'featureSelection'
-        FS = featureSelection(X, E, Y, featureNames, lambdas, outputDir, numIter);
+                       
         MP = FS;
-        
+          
     case 'RankingsSeveralLambdas'
         %% UNDER DEVELOPMENT
         
@@ -151,7 +265,7 @@ switch analysisType
         %inputs:
         %geneSelection(X, modifiedE, Y, geneNames, lambdas, outputRandomNetwork, geneIndex);
         
-        randomiseNetwork(X100, E100Nodes, Y, featureNames, lambdas, outputDir, ...
+        randomiseNetwork(X100, E100Nodes, Y, featureNames, lambda, outputDir, ...
             geneIndex, elementToChange, typeOfChange, perc, numRandomNetworks);
         
     case 'ErdosRenyi'
@@ -182,7 +296,7 @@ switch analysisType
         %inputs:
         %geneSelection(X, modifiedE, Y, geneNames, lambdas, outputRandomNetwork, geneIndex);
         
-        ErdosRenyiNetwork(X100, E100Nodes, Y, geneNames100, lambdas, outputDir, ...
+        ErdosRenyiNetwork(X100, E100Nodes, Y, geneNames100, lambda, outputDir, ...
             geneIndex, numRandomNetworks, algorithm);
             
 end
