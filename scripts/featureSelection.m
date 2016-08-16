bc464cebee72bde9ab5421b89f7ab73e3a1d8aed
PerlaTroncosoRey <perlatroncosorey@gmail.com>
2016-08-16 17:15:43 +0100

    added: returns the interceptor (learned from regression); removed: saving results to file

--- scripts/featureSelection.m
+++ scripts/featureSelection.m
@@ -1,4 +1,4 @@
-function FS = featureSelection(X, E, Y, Annot, lambda, outputDir, numIter)
+function [FS, vts] = featureSelection(X, E, Y, featureAnnot, lambda, numIter)
 % Feature selection using MultiPEN
 
 
@@ -18,12 +18,12 @@ function FS = featureSelection(X, E, Y, Annot, lambda, outputDir, numIter)
 %% Feature Selection with MultiPEN 
 % (number of folds is set to 1 to use all samples)
 %[weights, ~, ~, ~] = cross_validation(X, E, Y, lambda, folds, numIter, outputDir);
-[weights, ~, ~, ~] = cross_validation(X, E, Y, lambda, 1, numIter, outputDir);
+[weights, vts, ~, ~] = cross_validation(X, E, Y, lambda, 1, numIter);
 weights(weights < 1e-8) = 0;
 
 %% Rank the feature selection
 FS = table();
-FS.name = Annot;
+FS.name = featureAnnot;
 FS.weight = weights;
 R = tiedrank(abs(weights));
 % Reverse the ranking order used in tiedrank
@@ -31,11 +31,3 @@ R = tiedrank(abs(weights));
 % and the minimum absoulute weight has the larges ranking
 n = numel(R);
 FS.ranking = n - (R - 1);
-
-
-
-%% Write feature selection (FS table) to file
-%fileName = [outputDir 'Rankings_lambda' num2str(lambda)];
-%fprintf('Writing feature selection to file: \n\t%s\n',fileName)
-%writetable(FS, [fileName '.txt'], 'delimiter', '\t');
-%save([fileName '.mat'], 'FS')
