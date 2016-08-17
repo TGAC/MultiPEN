36eef540e5011cdf8abc4e7945efce2d8bed0370
PerlaTroncosoRey <perlatroncosorey@gmail.com>
2016-08-17 15:14:05 +0100

    removing sections under development to new file: MultiPEN_further_development.m; editing file name created by feature selection to remove work "genes" from files for a) higher-in-cases and b) higher-in-control

--- scripts/MultiPEN.m
+++ scripts/MultiPEN.m
@@ -3,7 +3,7 @@ function MP = MultiPEN(analysisType, saveResults, varargin)
 %    lambdas, folds, numIter)
 % Function to perform analysis of omics data using MultiPEN and
 % the different type of analysis (specified by parameter 'analysisType')
-% MultiPEN 0.0.1 computes feature selection from transcritpomics and 
+% MultiPEN 0.0.2 computes feature selection from transcritpomics and 
 % metabolomics data
 % Written by Perla Rey, Agust 2016
 %
@@ -227,131 +227,16 @@ switch analysisType
             dlmwrite([fileName '.txt'], vts, 'delimiter', '\t');    
             
             %% write separate tables for higherControl an higherCases
-            fileName = [outputDir 'MultiPEN-Rankings_lambda' num2str(lambda) '_genes-higher-in-control.txt'];
+            fileName = [outputDir 'MultiPEN-Rankings_lambda' num2str(lambda) '_higher-in-control.txt'];
             fprintf('Writing feature selection to file: \n\t%s\n',fileName)
             writetable(higherControl, fileName, 'delimiter', '\t');
             
-            fileName = [outputDir 'MultiPEN-Rankings_lambda' num2str(lambda) '_genes-higher-in-cases.txt'];
+            fileName = [outputDir 'MultiPEN-Rankings_lambda' num2str(lambda) '_higher-in-cases.txt'];
             fprintf('Writing feature selection to file: \n\t%s\n',fileName)
             writetable(higherCases, fileName, 'delimiter', '\t');
             %
         end
                        
         MP = FS;
-          
-    case 'RankingsSeveralLambdas'
-        %% UNDER DEVELOPMENT
-        
-    
-    case 'RandomiseNetwork'                
-        %% UNDER DEVELOPMENT        
-        outputDir = ['cuffnorm_output/string/GenePEN_results/' samplesForAnalysis '/' ];
-        
-        %load geneIndex
-        load('cuffnorm_output/string/geneList_Index_1perRow.mat')        
-                
-        %elementToChange   can be 'node' or 'edge'
-        %typeOfChange      can be 'swap', 'delete'
-        %percentage of elements to change 
-        %test with just first 100 genes and 
-        %corresponding network (variable E100Nodes):
-        load('cuffnorm_output/string/GenePEN_results/test2/E100Genes.mat')
-        X100 = X(:,1:100);
-        featureNames = featureNames(1:100,:);  
-        elementToChange = 'edge';
-        typeOfChange = 'swap';
-        perc = .40;   
-        numRandomNetworks = 5;
-            
-        %randomiseNetwork will run geneSelection which needs the following
-        %inputs:
-        %geneSelection(X, modifiedE, Y, geneNames, lambdas, outputRandomNetwork, geneIndex);
-        
-        randomiseNetwork(X100, E100Nodes, Y, featureNames, lambda, outputDir, ...
-            geneIndex, elementToChange, typeOfChange, perc, numRandomNetworks);
-        
-    case 'ErdosRenyi'
-        %% UNDER DEVELOPMENT 
-        outputDir = ['cuffnorm_output/string/GenePEN_results/' samplesForAnalysis '/RenyiErdos/' ]; 
-        %load geneIndex
-        load('cuffnorm_output/string/geneList_Index_1perRow.mat')
-        %test with just first 100 genes and 
-        %corresponding network (variable E100Nodes):
-        %load network
-        %load('cuffnorm_output/string/GenePEN_results/test2/E100Genes.mat')
-        X100 = X(:,1:100);
-        geneNames100 = featureNames(1:100,:);
-        numRandomNetworks = 5;       
-        algorithm = 'MIT';
-        %get the corresponding network for those 100 genes
-        E100Nodes = [];
-        for in100 = 1 : numel(E(:,1))
-            indxs1 = find(geneNames100.index == E(in100,1));
-            indxs2 = find(geneNames100.index == E(in100,2));                        
-            if ~isempty(indxs1) && ~isempty(indxs2)
-                E100Nodes(end+1,:) = [indxs1 indxs2 E(in100,3)]; 
-            end
-        end
-        
-        %run RenyiErdos test
-        %   RenyiErdos test will run geneSelection which needs the following
-        %inputs:
-        %geneSelection(X, modifiedE, Y, geneNames, lambdas, outputRandomNetwork, geneIndex);
-        
-        ErdosRenyiNetwork(X100, E100Nodes, Y, geneNames100, lambda, outputDir, ...
-            geneIndex, numRandomNetworks, algorithm);
-            
+
 end
-
-
-%%
-% % function RankingsSeveralLambdas()
-% % % Are the genes ranked consistently across lambdas?
-% % %load rankings for first lambda
-% % %files are save in file with name: weights_lambda0.1
-% % %gene ranking
-% % %tiedrank() ranks values in increasing order (larger value, larger rank)
-% % % load weights for current lambda
-% % fileName = [outputDir 'weights_lambda' num2str(lambdas(1)) '.mat'];
-% % load(fileName) %loads the weights
-% % Rankings = zeros(numel(weights),numel(lambdas)*2);
-% % jj=1;
-% % for ii = 1:+2:numel(lambdas)*2   %increments by two to add weights and rankings
-% %     fileName = [outputDir 'weights_lambda' num2str(lambdas(jj)) '.mat'];
-% %     load(fileName) %loads the weights
-% %     Rankings(:,ii) = weights;
-% %     R = tiedrank(weights);
-% %     % Reverse the ranking order used in tiedrank
-% %     n = numel(R);
-% %     Rankings(:,ii+1) = n - (R - 1);    
-% %     jj = jj+1;
-% % end
-% % 
-% % %load genes.attributes (variable genes_attributes)
-% % load('cuffnorm_output/genes_attributes_18samples.mat')
-% % %get only those the rows whose trackingID is on the list
-% % % expression.trackingID
-% % display('Obtaining short names...')
-% % locShortNames = zeros(numel(geneNames.trackingID),1);
-% % for ii = 1: numel(geneNames.trackingID)
-% %     locShortNames(ii) = find(strcmp(genes_attributes.tracking_id, geneNames.trackingID(ii)));    
-% % end
-% % display('Done!')
-% % shortName = genes_attributes.gene_short_name(locShortNames);
-% % 
-% % Rankings = table(geneNames.trackingID, shortName, Rankings(:,1), Rankings(:,2), Rankings(:,3), Rankings(:,4), ...
-% %     Rankings(:,5), Rankings(:,6), Rankings(:,7), Rankings(:,8));% ...
-% % %    Rankings(:,9), Rankings(:,10), Rankings(:,11), Rankings(:,12) ); %, ...
-% % % I wrote the column names manually directly into the text file
-% % % since a name of the sort: "weight_lambda1e-13" is not valid
-% % % The .mat file has columns names with the minus symbol (-) removed
-% % %     'VariableNames', {'trackingID' 'shortName' ...
-% % %     ['weight_lambda' num2str(lambdas(1),13)] ['ranking_lambda' num2str(lambdas(1))] ...
-% % %     ['weight_lambda' num2str(lambdas(2))] ['ranking_lambda' num2str(lambdas(2))] ...
-% % %     ['weight_lambda' num2str(lambdas(3))] ['ranking_lambda' num2str(lambdas(3))] ...
-% % %     ['weight_lambda' num2str(lambdas(4))] ['ranking_lambda' num2str(lambdas(4))] ...
-% % %     ['weight_lambda' num2str(lambdas(5))] ['ranking_lambda' num2str(lambdas(5))] ...
-% % %     ['weight_lambda' num2str(lambdas(6))] ['ranking_lambda' num2str(lambdas(6))] });
-% % 
-% % writetable(Rankings, [outputDir 'Rankings.txt'], 'delimiter', '\t')
-% % save([outputDir 'Rankings.mat'], 'Rankings')
