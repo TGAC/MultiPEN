46d868ebca7ff11d9b49b74090baae43d21c0475
PerlaTroncosoRey <perlatroncosorey@gmail.com>
2016-08-25 22:07:14 +0100

    enrichment analysis with GO, script to run with Rscript

--- scripts/pathwayAnalysis/enrichmentGO.R
+++ scripts/pathwayAnalysis/enrichmentGO.R
@@ -1,100 +1,107 @@
-  args = commandArgs(trailingOnly=TRUE)
+# TEST:
+# file with example data:
+# dataFile <- '~/Documents/Projects/multipen/2016-08-16_MultiPEN_v002/testing_MultiPEN/ExampleOutputs/MultiPEN-Rankings_lambda0.1.txt'
 
-  # test if there is at least one argument: if not, return an error
-  if (length(args)==0) {
-    stop("At least one argument must be supplied (input file).n", call.=FALSE)
-  } else if (length(args)==1) {
-    # default output file
-    args[2] = "output/"
-  }
-  
-  dataFile <- args[1]
-  cat(sprintf("Loading data from file: %s\n", dataFile))
+args = commandArgs(trailingOnly=TRUE)
 
-  outputDir <- args[2]
-  if(!dir.exists('output')){
-    cat(sprintf("Creating output directory: %s\n", outputDir))
-    dir.create("output/")
-  }
+# test if there is at least one argument: if not, return an error
+if (length(args)==0) {
+  stop("At least one argument must be supplied (input file).n", call.=FALSE)
+} else if (length(args)==1) {
+  # default output file
+  args[2] = "output_MultiPEN/enrichment-GO/"
+}
 
+dataFile <- args[1]
+cat(sprintf("Loading data from file: %s\n", dataFile))
 
-#GSEAnalysis <- function(dataFile){
-  # example data:
-  # dataFile <- '~/Documents/Projects/multipen/2016-08-16_MultiPEN_v002/testing_MultiPEN/ExampleOutputs/MultiPEN-Rankings_lambda0.1.txt'
-  getwd()
-  #path <- '/usr/users/ga004/troncosp/apps/multipen/pathwayAnalysis/'
-  #setwd(path)
-  #dataFile <- "MultiPEN-Rankings_lambda0.1.txt"
-  data <- read.table(dataFile, header=TRUE, sep = "\t", stringsAsFactors=FALSE)
-  cat(sprintf("Number of features: %i\n",nrow(data)))
+outputDir <- args[2]
+if(!dir.exists(outputDir)){
+  cat(sprintf("Creating output directory: %s\n", outputDir))
+  dir.create(outputDir)
+}
+
+# Load file with data for analysis - with columns: []
+data <- read.table(dataFile, header=TRUE, sep = "\t", stringsAsFactors=FALSE)
+cat(sprintf("Number of features: %i\n",nrow(data)))
 
 
-  library(clusterProfiler)
-  library(BBmisc)
-  library(GO.db)
+library(clusterProfiler)
+library(BBmisc)
+library(GO.db)
 
-  D <- sortByCol(data, 'ranking')
-  D <- D[D[,2]!=0,]
-  D <- D[,c(1,3)]  # Only interested in the first three columns [name, ranking]
-  entrez<-bitr(D$name, fromType="SYMBOL", toType="ENTREZID", annoDb="org.Hs.eg.db",drop = FALSE)
-  ranked<-merge(D,entrez,by.x='name',by.y='SYMBOL')
+D <- sortByCol(data, 'ranking')
+D <- D[D[,2]!=0,]
+D <- D[,c(1,3)]  # Only interested in the first three columns [name, ranking]
+entrez<-bitr(D$name, fromType="SYMBOL", toType="ENTREZID", annoDb="org.Hs.eg.db",drop = FALSE)
+ranked<-merge(D,entrez,by.x='name',by.y='SYMBOL')
 
-  cat(sprintf("Performing over-representation analysis (enrichGO) ...  "))
-  cat(sprintf("Results saved to folder: %s\n", outputDir))
-  
-  #Enrichment for subontology BP (Biological Processes)
-  subclassOnt <- "BP"
-  enrichment_BP <- enrichGO(ranked$ENTREZID, organism='human', ont=subclassOnt, readable=TRUE)
-  enrichmentSummary_BP <- summary(enrichment_BP)
-  head(summary(enrichment_BP))
-  if(nrow(enrichmentSummary_BP)>0){
-    aux <- cbind(enrichmentSummary_BP, "BP")
-    colnames(aux)[10]<- 'subontology'
-    enrichmentSummary_BP <- aux        
-  }  
-  #add to results: enrichment for BP category
-  results <- enrichmentSummary_BP
-  
-  #Enrichment for subontology MF (Molecular Function)
-  subclassOnt <- "MF"
-  enrichment_MF <- enrichGO(ranked$ENTREZID, organism='human', ont=subclassOnt, readable=TRUE)
-  enrichmentSummary_MF <- summary(enrichment_MF)
-  head(summary(enrichment_MF)) 
-  if(nrow(enrichmentSummary_MF)>0){
-    aux <- cbind(enrichmentSummary_MF, "MF")
-    colnames(aux)[10]<- 'subontology'
-    enrichmentSummary_MF <- aux       
-    #add to results: enrichment for BP category
-    results <- rbind(results, enrichmentSummary_MF)
-  }  
-  
-  
-  #Enrichment for subclass CC (Cellular Cellular Component)
-  subclassOnt <- "CC"
-  enrichment_CC <- enrichGO(ranked$ENTREZID, organism='human', ont=subclassOnt, readable=TRUE)
-  enrichmentSummary_CC <- summary(enrichment_CC)
-  head(summary(enrichment_CC))
-  if(nrow(enrichmentSummary_CC)>0){
-    aux <- cbind(enrichmentSummary_CC, "CC")
-    colnames(aux)[10]<- 'subontology'
-    enrichmentSummary_CC <- aux
-    #add to results: enrichment for BP category
-    results <- rbind(results, enrichmentSummary_CC)
-  }
-  
-  
-  #if(nrow(results)>0){
-    #write results to file: 
-    fileName <- paste(outputDir, "enrichGO_subcategories.txt", sep = "")
-    cat(sprintf("writing results to file: %s\n", fileName))
-    write.table(results, fileName, sep = '\t', row.names = FALSE)
-  #}
-  
-  fileName <- paste(outputDir, 'enrichmentGO.pdf', sep = "")
-  pdf(fileName)
-  par(mfcol=c(1,3))
-  barplot(enrichment_BP, drop=TRUE, showCategory=20)
-  barplot(enrichment_MF, drop=TRUE, showCategory=20)
-  barplot(enrichment_CC, drop=TRUE, showCategory=20)
-  dev.off()
-  
+cat(sprintf("Performing over-representation analysis (enrichGO) ...  "))
+cat(sprintf("Results saved to folder: %s\n", outputDir))
+
+#Enrichment for subontology BP (Biological Process)
+subclassOnt <- "BP"
+enrichment_BP <- enrichGO(ranked$ENTREZID, organism='human', ont=subclassOnt, readable=TRUE)
+enrichmentSummary_BP <- summary(enrichment_BP)
+head(summary(enrichment_BP))
+if(nrow(enrichmentSummary_BP)>0){
+  aux <- cbind(enrichmentSummary_BP, "BP")
+  colnames(aux)[10]<- 'subontology'
+  enrichmentSummary_BP <- aux        
+}  
+#add to results: enrichment for BP category
+results <- enrichmentSummary_BP
+
+#Enrichment for subontology MF (Molecular Function)
+subclassOnt <- "MF"
+enrichment_MF <- enrichGO(ranked$ENTREZID, organism='human', ont=subclassOnt, readable=TRUE)
+enrichmentSummary_MF <- summary(enrichment_MF)
+head(summary(enrichment_MF)) 
+if(nrow(enrichmentSummary_MF)>0){
+  aux <- cbind(enrichmentSummary_MF, "MF")
+  colnames(aux)[10]<- 'subontology'
+  enrichmentSummary_MF <- aux       
+  #add to results: enrichment for MF category
+  results <- rbind(results, enrichmentSummary_MF)
+}  
+
+
+#Enrichment for subclass CC (Cellular Component)
+subclassOnt <- "CC"
+enrichment_CC <- enrichGO(ranked$ENTREZID, organism='human', ont=subclassOnt, readable=TRUE)
+enrichmentSummary_CC <- summary(enrichment_CC)
+head(summary(enrichment_CC))
+if(nrow(enrichmentSummary_CC)>0){
+  aux <- cbind(enrichmentSummary_CC, "CC")
+  colnames(aux)[10]<- 'subontology'
+  enrichmentSummary_CC <- aux
+  #add to results: enrichment for CC category
+  results <- rbind(results, enrichmentSummary_CC)
+}
+
+# modify column names for consistency for valid MATLAB identifiers
+# change: p.adjust to pAdjust
+aux <- colnames(results)
+aux[c(5,6,7)] <- c("pValue", "pAdjust", "qValue")
+colnames(results) <- aux
+
+#write results to file: 
+fileName <- paste(outputDir, "enrichment-GO.txt", sep = "")
+cat(sprintf("writing results to file: %s\n", fileName))
+write.table(results, fileName, sep = '\t', row.names = FALSE)
+
+
+fileName <- paste(outputDir, 'enrichment-GO_BP.pdf', sep = "")
+pdf(fileName)
+barplot(enrichment_BP, showCategory=20)
+dev.off()
+
+fileName <- paste(outputDir, 'enrichment-GO_MF.pdf', sep = "")
+pdf(fileName)
+barplot(enrichment_MF, drop=TRUE, showCategory=20)
+dev.off()
+
+fileName <- paste(outputDir, 'enrichment-GO_CC.pdf', sep = "")
+pdf(fileName)
+barplot(enrichment_CC, showCategory=20)
+dev.off()
