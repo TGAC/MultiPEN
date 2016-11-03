8c24b196ee440b1a324cadce631487880a7af013
Perla Troncoso Rey <perlatroncosorey@gmail.com>
2016-11-03 11:20:24 +0000

    removed tests with public dataset

--- scripts/2016-08-22_Test-for_GSE46300_MTBLS174/scripts/saveMetabolomicsAsTxt.R
+++
@@ -1,6 +0,0 @@
-
-fileName = '~/Documents/Public-Datasets/2016-8-24_Fatty-Liver-Disease_Gene-Expression_Metabolomics/Original-Data/MetaboLights_ MTBLS174/MTBLS174_20160823_115437/m_hna_fld_metabolite_profiling_NMR_spectroscopy_v2_maf.tsv'
-metabolomics <- read.delim(file = fileName, sep = "\t", stringsAsFactors = FALSE)
-
-fileName = '~/Documents/Public-Datasets/2016-8-24_Fatty-Liver-Disease_Gene-Expression_Metabolomics/Processed-Data/MetaboLights/m_hna_fld_metabolite_profiling_NMR_spectroscopy_v2_maf.txt'
-write.table(metabolomics, fileName, sep = "\t", row.names = FALSE)
