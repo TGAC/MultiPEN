f09e0402ea64354937e6d4a5fe5a61058645b84d
Perla Troncoso Rey <perlatroncosorey@gmail.com>
2016-11-08 14:27:59 +0000

    removed comments; updated description of input parameters

--- scripts/subnetwork4ExpressionData.m
+++ scripts/subnetwork4ExpressionData.m
@@ -2,11 +2,10 @@ function E = subnetwork4ExpressionData(network, XAnnotation)
 % Function to create the subnetwork with only nodes that correspond to the 
 % features for the expression data.
 
-% network   The full network. It's a table with three columns: source, source and weight
-% X     The expression matrix n-by-p, where p is the number of features
-%       (genes and/or metabolites)
+% network   The full network. It's a table with three columns: source, target and weight
 % XAnnotation The p-by-1 cell vector with the feature's names 
 %       For genes this is the geneSymbols, for metabolites is the Chemical IDs
+%       preceded by the string 'CHEBI:', e.g. 'CHEBI:10049'
 
 %% list of unique nodes in the network
 
@@ -37,26 +36,6 @@ if nn ~= mm
     error('There are duplicated feature names')
 end
 
-% % %% Build Xprime and XAnnotationPrime for ONLY the features 
-% % %  (genes/metabolites) that are in the network 
-% % XAnnotationPrime = {};
-% % Xprime = [];
-% % % check what elements in XAnnotation are in the 'nodes' list
-% % for ii = 1:numel(XAnnotation)
-% %     current = XAnnotation{ii};
-% %     index = find(strcmp(current, nodes.IDs));
-% %     %check that the current feature is not already on the list
-% %     if isempty(find(strcmp(current, XAnnotationPrime)))
-% %         if ~isempty(index)
-% %             XAnnotationPrime{end+1,1} = current;
-% %             Xprime(:,end+1) = X(:,ii);            
-% %         end
-% %     else
-% %         error('There are duplicated feature names')
-% %     end
-% % end
-% % clear ii current index
-% % % List of filtered data is now on Xprime and XAnnotationPrime
 
 %% Build E 
 % Nodes are represented by the index according to XAnnotation 
