d3caf93352e487384a4c75e68adff0c3ca466cd0
Perla Troncoso Rey <perlatroncosorey@gmail.com>
2017-03-31 14:06:51 +0100

    edited pcaExpressionData.m: display plot for pca; added case to plot pca for ten groups

--- scripts/pcaExpressionData.m
+++ scripts/pcaExpressionData.m
@@ -20,14 +20,16 @@ x = zscore(scores(:,1));
 y = zscore(scores(:,2));
 z = zscore(scores(:,3));
 
-%figure()
+figure()
 c = unique(groupLabels);  % c contains the unique zones
 switch numel(c)
     case 2
-        figure('Visible','off')
         gscatter3(x,y,z,groupLabels, {'b', 'm'}, {'.','.'}, 20);
     case 4 
         gscatter3(x,y,z,groupLabels, {'b', 'k', 'm', 'g'}, {'.','.','.','.'}, 20);
+    case 10
+        gscatter3(x,y,z,groupLabels, {'b', 'b', 'k', 'k', 'm', 'm', 'g', 'g', 'r', 'r'}, ...
+            {'*','s','*','s','*','s','*','s','*','s'});
 end
 title(titleGraph)
 
@@ -35,39 +37,55 @@ xlabel(['PC-1(' num2str(round(pcvars(1)/sum(pcvars)*100)) '% variance)'])
 ylabel(['PC-2(' num2str(round(pcvars(2)/sum(pcvars)*100)) '% variance)'])
 zlabel(['PC-3(' num2str(round(pcvars(3)/sum(pcvars)*100)) '% variance)'])
 
+if ~strcmp(outputDir,'false')
+    % Check if output directory exists
+    if exist(outputDir, 'dir') ~= 7
+        mkdir(outputDir)
+    end
 
-% Check if output directory exists
-if exist(outputDir, 'dir') ~= 7
-    mkdir(outputDir)
+    % Save figure
+    fileName = [outputDir 'pca_three_components.png'];
+    saveas(gcf, fileName)
+    fprintf('\tFigure save to file: \n\t%s\n', ...
+        fileName)
 end
-
-% Save figure
-fileName = [outputDir 'pca_three_components.png'];
-saveas(gcf, fileName)
-fprintf('\tFigure save to file: \n\t%s\n', ...
-    fileName)
 
 %% 
 % Plot first two components
 
-%[coeff,scores,latent,tsquared,explained] = pca(zscore(table2array(mLevelsBenign)'));
-
 % plot the first two principal components
-%figure()
-indx = find(strcmp(groupLabels, c(1)));
-plot(scores(indx,1),scores(indx,2),'+m')
-indx = find(strcmp(groupLabels, c(2)));
-hold on, plot(scores(indx,1),scores(indx,2),'+b'), hold off
-legend(c)
+figure()
+switch numel(c)
+    case 2
+        gscatter(x,y,groupLabels', 'bm', '*s');
+    case 4
+        gscatter(x,y, groupLabels', 'bkmr', '*sod')
+    case 10
+        gscatter(x,y,groupLabels', 'bbkkmmggrr', '*s*s*s*s*s');
+end
+
 xlabel(['PC-1(' num2str(round(pcvars(1)/sum(pcvars)*100)) '% variance)'])
 ylabel(['PC-2(' num2str(round(pcvars(2)/sum(pcvars)*100)) '% variance)'])
 title(titleGraph)
+        
+        
+%for only the first two groups
+% indx = find(strcmp(groupLabels, c(1)));
+% plot(scores(indx,1),scores(indx,2),'+m')
+% indx = find(strcmp(groupLabels, c(2)));
+% hold on, plot(scores(indx,1),scores(indx,2),'+b'), hold off
+% legend(c)
+% xlabel(['PC-1(' num2str(round(pcvars(1)/sum(pcvars)*100)) '% variance)'])
+% ylabel(['PC-2(' num2str(round(pcvars(2)/sum(pcvars)*100)) '% variance)'])
+% title(titleGraph)
 
 % Save figure
-fileName = [outputDir 'pca_two_components.png'];
-saveas(gcf, fileName)
-fprintf('\tFigure save to file: \n\t%s\n', ...
-    fileName)
+if ~strcmp(outputDir,'false')
+    fileName = [outputDir 'pca_two_components.png'];
+    saveas(gcf, fileName)
+    fprintf('\tFigure save to file: \n\t%s\n', ...
+        fileName)
+end
 
 
 %% 
@@ -75,15 +93,14 @@ fprintf('\tFigure save to file: \n\t%s\n', ...
 % biplot of the pca coefficients 
 if nargin > 3
     %biplot
-    %figure
+    figure()
     biplot(coeff(:,1:3),'scores',scores(:,1:3),'varlabels',annot);
     title(titleGraph)
     
     % Save figure
-    fileName = [outputDir 'pca_biplot.png'];
-    saveas(gcf, fileName)
-    fprintf('\tFigure save to file: \n\t%s\n', ...
-    fileName)
+    if ~strcmp(outputDir, 'false')
+        fileName = [outputDir 'pca_biplot.png'];
+        saveas(gcf, fileName)
+        fprintf('\tFigure save to file: \n\t%s\n', fileName)
+    end
 end
-
-close all hidden
