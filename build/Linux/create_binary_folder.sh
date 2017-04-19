cdf573f1569f5ef5d28c3eae8f75ae35258e6875
Perla Troncoso Rey <perlatroncosorey@gmail.com>
2017-04-19 16:07:32 +0100

    updating information on the new version for MultiPEN executable

--- build/Linux/create_binary_folder.sh
+++ build/Linux/create_binary_folder.sh
@@ -3,47 +3,39 @@
 # It copies the following files:
 # - MATLAB files for current binary
 # - R scripts for pathway analysis
-# - Example files (inputs and outputs) and
 # - Scripts to test MultiPEN with example files
 # To a new folder for MultiPEN's current version (i.e., MultiPEN_current-version_Linux)
 
 # Original files are located in:
 #   Binary: "MultiPEN/MultiPEN_executable_development/mcc_files_Linux/"
 #   R scripts for pathway analysis: "MultiPEN/scripts/pathwayAnalysis/"
-#   Example Input Files: "MultiPEN/MultiPEN_executable_development/ExampleInputs/"
-#   Example Output Files: "MultiPEN/MultiPEN_executable_development/ExampleOutputs/"
 #   Bash scripts to run examples: "MultiPEN/scripts/tests/"
 
 
-# Currrent Version: MultiPEN_v001_Linux
+# Currrent Version: MultiPEN_v003_Linux
 # Content:
 #   MultiPEN (the application)
+#   compileNetworkStringDB.R
 #   pathwayAnalysis/
 #      enrichmentGO_sortedList.R
 #      enrichmentGO.R
 #   example_cross_validation.sh
-#   example_feature_selection.sh   
-#   ExampleInputs
-#      expressionData.txt
-#      interactionMatrix.txt
-#      sampleClass.txt
-#   ExampleOutputs/
-#      CrossValidation/
-#         cross-validation_stats.txt
-#      FeatureSelection/
-#         MultiPEN-feature-selection_config.txt
-#         MultiPEN-performance_feature-selection_lambda0.0001.txt
-#         MultiPEN-Rankings_lambda0.0001_higher-in-cases.txt
-#         MultiPEN-Rankings_lambda0.0001_higher-in-control.txt
-#         MultiPEN-Rankings_lambda0.0001.txt
-#         MultiPEN-vts_lambda0.0001.txt 
+#   example_feature_selection.sh
+#   example_pca.sh
+#   example_hierarchical_clustering.sh
+#   example_enrichment_GO.sh
+#   example_enrichment_KEGG.sh
+#   example_STRINGdb.sh
+
+# folderName is the target folder for current version of MultiPEN
+# change the following accordingly
+folderName="MultiPEN_v003_Linux"
 
 origin="mcc_files_Linux/"   # directory containing binary
 rscripts="../../scripts/pathwayAnalysis/"  # directory containing all R scripts for pathway analysis
 exampleScripts="../../scripts/tests/Linux/"  #path to bash scripts to run examples
+stringdbRscript="../../scritps/compileNetworkStringDB.R"  # R script to compile network with STRINGdb
 
-
-folderName="MultiPEN_v001_Linux"
 
 if [ ! -d "$folderName/" ]
 then
@@ -65,11 +57,11 @@ chmod 755 ${folderName}/run_MultiPEN.sh
 # Create pathwayAnalysis folder 
 mkdir ${folderName}/pathwayAnalysis
 # Copy all R scripts for pathway analysis
-cp -Rp ${rscripts}*.R ${folderName}/pathwayAnalysis/  # copies all R scripts
+cp -Rp ${rscripts}*.R ${folderName}/pathwayAnalysis/  # copy all R scripts
+# Copy R script to compile network using STRINGdb
+cp -Rp $stringdbRscript ${folderName}
 
 
-# Copy all example files and scripts to run examples
-cp -Rp ExampleInputs/ ${folderName}/ExampleInputs/
-cp -Rp ExampleOutputs/ ${folderName}/ExampleOutputs/
+# Copy all scripts to run examples
 cp -p ${exampleScripts}example_*.sh ${folderName}/  #copies all bash scripts to run examples
 chmod 755 ${folderName}/example_*.sh
