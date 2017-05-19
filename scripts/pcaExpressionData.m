274a50157c0d6d76edbf79571e0f33a067b1663b
Perla Troncoso Rey <perlatroncosorey@gmail.com>
2017-05-19 13:10:53 -0600

    edited pcaExpressionData.m to 1) added input parameter (flag) to decide whether to show biplot; 2) order alphabetically the data according to their groups before plotting 2 componentsl (to be consistent with tcolour and markers in plot of 3 components

--- scripts/pcaExpressionData.m
+++ scripts/pcaExpressionData.m
@@ -1,16 +1,19 @@
-function pcaExpressionData(X, groupLabels, titleGraph, annot, outputDir)
-% Performs PCA
-% Plots first three and two principal components
+function pcaExpressionData(X, groupLabels, titleGraph, annot, outputDir, varargin)
+% PCA: Plots Principal Components
 %
 % Inputs:
 % X is the n-by-p data matrix for n samples, p features
 % groupLabels
 % Example of zone's Labels
-% labelZones=[{'PZ'}, {'PZ'}, {'PZ'}, {'PZ'}, {'PZ'}, {'PZ'}, {'PZ'}, ...
-%    {'TZ'}, {'TZ'}, {'TZ'}, {'TZ'}, {'TZ'}, {'TZ'}, {'TZ'}];
+% groupLabels=[{'group1'}, {'group1'}, {'group1'}, {'group1'}, ...
+%    {'group2'}, {'group2'}, {'group2'}, {'group2'}];
 % titleGraph
-% annot     Annotation for features (genes/metabolites)
+% annot     Annotation for features (genes and/or metabolites)
 % outputDir  Location to save the figures
+
+if nargin < 6
+    BiplotFlag = 0;
+end
 
 [coeff, scores, pcvars] = pca(X);
 
@@ -21,7 +24,7 @@ y = zscore(scores(:,2));
 z = zscore(scores(:,3));
 
 figure()
-c = unique(groupLabels);  % c contains the unique zones
+c = unique(groupLabels);  % c contains the unique groups
 switch numel(c)
     case 2
         gscatter3(x,y,z,groupLabels, {'b', 'm'}, {'.','.'}, 20);
@@ -52,32 +55,30 @@ end
 
 %% 
 % Plot first two components
+% To be consistent with the colour and markers per group plot by gscatter3
+% (which orders the groups alphabetically), order the unique groups 
+% alphabetically before calling gscatter
+indx = [];
+for g = 1:numel(c)
+    indx = [indx find(strcmp(groupLabels, c(g)))];
+end
+xT = x(indx); yT = y(indx); groupLabelsT = groupLabels(indx);
+
 
 % plot the first two principal components
 figure()
 switch numel(c)
     case 2
-        gscatter(x,y,groupLabels', 'bm', '*s');
+        gscatter(xT, yT, groupLabelsT', 'bm', '*s');
     case 4
-        gscatter(x,y, groupLabels', 'bkmr', '*sod')
+        gscatter(xT, yT, groupLabelsT', 'bkmr', '*sod')
     case 10
-        gscatter(x,y,groupLabels', 'bbkkmmggrr', '*s*s*s*s*s');
+        gscatter(xT, yT, groupLabelsT', 'bbkkmmggrr', '*s*s*s*s*s');
 end
 
 xlabel(['PC-1(' num2str(round(pcvars(1)/sum(pcvars)*100)) '% variance)'])
 ylabel(['PC-2(' num2str(round(pcvars(2)/sum(pcvars)*100)) '% variance)'])
 title(titleGraph)
-        
-        
-%for only the first two groups
-% indx = find(strcmp(groupLabels, c(1)));
-% plot(scores(indx,1),scores(indx,2),'+m')
-% indx = find(strcmp(groupLabels, c(2)));
-% hold on, plot(scores(indx,1),scores(indx,2),'+b'), hold off
-% legend(c)
-% xlabel(['PC-1(' num2str(round(pcvars(1)/sum(pcvars)*100)) '% variance)'])
-% ylabel(['PC-2(' num2str(round(pcvars(2)/sum(pcvars)*100)) '% variance)'])
-% title(titleGraph)
 
 % Save figure
 if ~strcmp(outputDir,'false')
@@ -91,7 +92,7 @@ end
 %% 
 %if annotation for each feature (gene/metabolite) is provided
 % biplot of the pca coefficients 
-if nargin > 3
+if nargin > 3 && BiplotFlag
     %biplot
     figure()
     biplot(coeff(:,1:3),'scores',scores(:,1:3),'varlabels',annot);
